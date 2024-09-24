0e364b884336e876c9fcb355fa4923911df9788f
SUPERBOLIDEIRL <SUPERBOLIDE>
2024-09-23 20:44:34 -0400

    Added STUDENT_DATA struct that contains the names of the students. Added functionality to read from the textfile of students and put objects of the struct into a vector space.

--- Assignment#2/Source.cpp
+++ Assignment#2/Source.cpp
@@ -1,4 +1,29 @@
-int main()
-{
-	return 1;
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+struct STUDENT_DATA {
+    std::string fn;
+    std::string ln;
+};
+
+int main() {
+    std::ifstream file("StudentData.txt");
+    std::vector<STUDENT_DATA> students;
+
+    if (file.is_open()) {
+        std::string line;
+        while (std::getline(file, line)) {
+            std::stringstream ss(line);
+            std::string fn, ln;
+
+            std::getline(ss, fn, ',');
+            std::getline(ss, ln);
+
+            students.push_back({ fn, ln });
+        }
+        file.close();
+    }
+    return 0;
 }
